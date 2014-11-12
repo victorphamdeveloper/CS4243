02cb9d0924b454eb0afbdf59609615da1908eab4
Bui Trong Nhan <imouto1994@gmail.com>
2014-11-13 04:24:12 +0800

    Modify path

--- src/cameraPathGenerator.py
+++ src/cameraPathGenerator.py
@@ -26,18 +26,18 @@ class CameraPathGenerator:
          ((position2.x, position2.y, position2.z), (orientation2.x, orientation2.y, orientation2.z)),...]
     """
     def generateCameraPath(self):
-        point1 = ((self.IMAGE_ORIGINAL_WIDTH * 1 / 2.0, 
-                   self.IMAGE_ORIGINAL_HEIGHT * 9 / 10.0, 
+        point1 = ((self.IMAGE_ORIGINAL_WIDTH * 2.5 / 5.0, 
+                   self.IMAGE_ORIGINAL_HEIGHT * 7.75 / 10.0, 
                    0), 0)
         point2 = ((self.IMAGE_ORIGINAL_WIDTH * 3 / 5.0, 
                    self.IMAGE_ORIGINAL_HEIGHT * 9 / 10.0, 
-                   200), np.pi / 16.0)
-        point3 = ((self.IMAGE_ORIGINAL_WIDTH * 1 / 2.0, 
+                   200), np.pi / 12.0)
+        point3 = ((self.IMAGE_ORIGINAL_WIDTH * 2.5 / 5.0, 
                    self.IMAGE_ORIGINAL_HEIGHT * 9 / 10.0, 
                    400), 0)
         point4 = ((self.IMAGE_ORIGINAL_WIDTH * 2 / 5.0, 
                    self.IMAGE_ORIGINAL_HEIGHT * 9 / 10.0, 
-                   600), -np.pi / 8.0)
+                   600), -np.pi / 6.0)
         keyPointsAndAngles = [point1, point2, point3, point4]
         generatedPoints = []
         
